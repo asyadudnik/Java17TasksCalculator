d4f1162fbad1f9d436e619567958125a27d7b768
asyadudnik <asyadudnik@hotmail.com>
2024-03-15 14:27:08 +0200

    Initial commit

---
+++ src/test/java/com/tasks/calculator/TasksCalculator/com/tasks/calculator/TasksCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tasks.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasksCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
