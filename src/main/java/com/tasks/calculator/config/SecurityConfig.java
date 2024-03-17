c305b46a60732a729c2e53b33c56581a17d3faef
asyadudnik <asyadudnik@hotmail.com>
2024-03-17 20:52:58 +0200

    init commit

--- src/main/java/com/tasks/calculator/config/SecurityConfig.java
+++ src/main/java/com/tasks/calculator/config/SecurityConfig.java
@@ -7,16 +7,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.persistence.EntityManagerFactory;
-import javax.sql.DataSource;
 import java.util.Properties;
-
-import static com.tasks.calculator.global.InstallConstants.*;
 
 @Configuration
 @EnableTransactionManagement
