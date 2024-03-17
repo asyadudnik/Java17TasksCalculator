097dcb63d72da203ede08bf83c856b5fcba3c542
asyadudnik <asyadudnik@hotmail.com>
2024-03-17 13:17:17 +0200

    init commit

--- src/main/java/com/tasks/calculator/controllers/global/HomeController.java
+++ src/main/java/com/tasks/calculator/controllers/global/HomeController.java
@@ -2,7 +2,6 @@ package com.tasks.calculator.controllers.global;
 
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.HashMap;
